19a49ef9743fa91a7d8b3b0f9fcf489a0217fcad
Jonatas Braz <jonatasbraz@gmail.com>
2019-09-07 15:41:33 -0300

    estilização do Dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,10 +1,47 @@
 import React from 'react';
-import api from '~/services/api';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
-  api.get('appointments');
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#FFF" />
+        </button>
+        <strong>31 de Maio</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#FFF" />
+        </button>
+      </header>
 
-  return <h1>Dashboard</h1>;
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Em aberto</span>
+        </Time>
+        <Time>
+          <strong>09:00</strong>
+          <span>Jonatas Braz</span>
+        </Time>
+        <Time available>
+          <strong>10:00</strong>
+          <span>Em aberto</span>
+        </Time>
+        <Time available>
+          <strong>11:00</strong>
+          <span>Em aberto</span>
+        </Time>
+        <Time available>
+          <strong>12:00</strong>
+          <span>Em aberto</span>
+        </Time>
+        <Time available>
+          <strong>13:00</strong>
+          <span>Em aberto</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
